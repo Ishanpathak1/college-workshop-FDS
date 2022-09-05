9906d7f7cb8bf4144d66154a17e91884dfd6450d
Ishan Pathak <ishanpathak@Ishans-MacBook-Pro.local>
2022-09-05 11:11:00 +0530

    practical2commands Issue-commands are not running error is-pandas module not found, tried pip , then command pip not found

---
+++ practical2commands.py
@@ -0,0 +1,8 @@
+import csv
+import pandas as pd
+
+with open('/Users/ishanpathak/Desktop/Workshop-foundation-of-data-science/college-workshop-FDS/sem5pract2new.csv' , mode='r') as file:
+    csvFile=csv.reader(file)
+
+df=pd.read_csv('sem5pract2new.csv')
+df
